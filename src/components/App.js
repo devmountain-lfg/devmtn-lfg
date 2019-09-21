0c3c4d6ed92dce7f3d3ce730b68bce47cece08a2
Paul Graves <pgraves@hsahealthplan.com>
2019-09-21 10:52:16 -0600

    i was going to work on the public page, but instead i just remove3d a duplicate route to the PublicPage component, and i changed the component name of the Public page from Publicpage to PublicPage

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import axios from "axios";
-import Publicpage from "./publicpage";
+import PublicPage from "./publicpage";
 import CreateUser from "./createuser";
 import UserSetup from "./usersetup";
 import CreateEvents from "./createevents";
@@ -22,15 +22,14 @@ function App() {
           <Route exact path="/" render={props => props.history.push('/public_page')} />
           {/* <Route path="/app" component={AuthenticatedRouts} /> */}
           <Route path="/create_user" component={CreateUser} />
-          <Route path="/" component={Reference} />
-          <Route path="/public_page" component={Publicpage} />
+          <Route path="/Reference" component={Reference} />
+          <Route path="/public_page" component={PublicPage} />
           <Route path="/home_page" component={Homepage} />
           <Route path="/calendar" component={Calendar} />
           <Route path="/manage_events" component={ManageEvents} />
           <Route path="/create_event" component={CreateEvents} />
           <Route path="/user_setup" component={UserSetup} />
           <Route path="/chats" component={Chats} />
-          <Route path="/public_page" component={Publicpage} />
           <Route path="/settings" component={Settings} />
         </Switch>
       </Router>
