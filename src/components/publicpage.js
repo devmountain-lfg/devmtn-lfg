eb7fdf10db17141415e6d2b1f5904a8cb70549fb
Taylor Woodman <taylor.woodman711@gmail.com>
2019-09-21 12:10:44 -0600

    no response.data

--- src/components/publicpage.js
+++ src/components/publicpage.js
@@ -23,15 +23,13 @@ class PublicPage extends Component {
       return <div></div>;
     });
     return (
-      <div className="main-container-ref">
+      <div className="publicpage-ref">
         <header className="header-ref">
           <Link to="/login">
             <button className="button-ref-medium">Sign In</button>
           </Link>
         </header>
-        <div className="events-ref">
-          <currentEvents />
-        </div>
+        <div className="events-ref"></div>
         <footer className="footer-ref">
           <button className="home-button-ref">Home</button>
           <button className="add-event-ref">+</button>
