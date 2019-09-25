6aaf0cca3af8e7468e1979eea83b19d265f46574
Paul Graves <pgraves@hsahealthplan.com>
2019-09-24 19:16:24 -0600

    Added View and Stored Proc for current events and creating events

--- db/procedures.sql
+++ db/procedures.sql
@@ -1,4 +1,5 @@
 DROP PROCEDURE IF EXISTS addnewuser;
+DROP PROCEDURE IF EXISTS createNewEvent;
 
 CREATE PROCEDURE addNewUser(
     first_name varchar(50), 
@@ -50,6 +51,48 @@ BEGIN
         
     INSERT INTO users(first_name,last_name,gender,email,phone_number,username,user_password)
     VALUES($1,$2,$3,$4,$5,$6,$7);
+    
+    RETURN;
+END;
+$$;
+
+
+CREATE PROCEDURE createNewEvent(
+    activity_id int, 
+    event_date TIMESTAMP,
+    is_public_event BOOLEAN,
+    max_player int,
+    creator_id int
+)
+LANGUAGE plpgsql    
+AS $$
+BEGIN
+    if $2 < NOW()
+        THEN 
+           RAISE NOTICE 'Invalid event date. Must be in the future';
+           RETURN;
+        END IF;
+    
+    if (select count(*) from activities a where a.activity_id = $1) < 1
+        THEN
+            RAISE NOTICE 'Invalid activity id';
+            RETURN;
+        END IF;
+        
+    if $4 < (select min_players_required from activities a where a.activity_id = $1)
+        THEN 
+            RAISE NOTICE 'Max Players is less than min players required';
+            RETURN;
+        END IF;
+        
+    if (select count(*) from users u where u.user_id = $5) < 1
+        THEN
+            RAISE NOTICE 'Invalid user_id for creator';
+            RETURN;
+        END IF;
+        
+    INSERT INTO events(activity_id,event_date,public_event,creator_id,max_players)
+    VALUES($1,$2,$3,$5,$4);
     
     RETURN;
 END;
