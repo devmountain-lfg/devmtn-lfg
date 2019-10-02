81380e7bde753d119ea13b7004711e605d998527
PeterFarnsworth23 <peter@farnsworth.us>
2019-10-01 19:37:11 -0600

    set calendar to pull from the database for the specified user and set so clicking the event will alert the user the event information

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -8,74 +8,50 @@ import "../styling/petercalendar.css";
 const localizer = momentLocalizer(moment);
 
 class calendar extends Component {
-  
   state = {
-    events: [
-      {
-        'title': 'All Day Event very long title',
-        'allDay': true,
-        'start': new Date(2019, 3, 0),
-        'end': new Date(2019, 3, 1)
-      },
-      {
-        'title': 'Long Event',
-        'start': new Date(2019, 3, 7),
-        'end': new Date(2019, 3, 10)
-      },
-    
-      {
-        'title': 'DTS STARTS',
-        'start': new Date(2019, 2, 13, 0, 0, 0),
-        'end': new Date(2019, 2, 20, 0, 0, 0)
-      },
-    
-      {
-        'title': 'DTS ENDS',
-        'start': new Date(2019, 10, 6, 0, 0, 0),
-        'end': new Date(2019, 10, 13, 0, 0, 0)
-      },
+    events: [}
 
-      {
-      'id': 1,
-      'title':"Test",
-      'start': new Date('2019-9-24 6:00'),
-      'end':new Date('2019-9-24 8:00'),
-    }]
-  
+  getEvents = async () => {
+    const response = await axios.get(`/events/${this.props.userInfo.user_id}`);
+    if (response.data.length > 0) {
+    {
+      this.setState({
+        events: response.data.map(item => ({
+          id: item.id,
+          title: item.activity_name,
+          start: new Date(item.event_date_start),
+          end: new Date(item.event_date_end),
+          userId: item.userId,
+          description: item.description
+        }))
+      });
+    };
+  }
   };
 
-// getEvents = async () => {
-//   const response = await axios.get("/events");
-//   console.log(response.data.length > 0) {
-//     this.setState({
-//      events: response.data.map(item => ({
-//        id: item.id,
-//        title: title,
-//        start: new Date(item.start),
-//        end: new Date(item.end),
-//        userId: item.userId,
-//        description: item.description
-//      }))
-//     });
-//   }
-// };
+  async componentDidMount() {
+    this.getEvents();
+  }
 
-// async componentDidMount() {
-//   this.getEvents();
-// }
+  // eventSelect = event => {
+  // const r = window(`Start Time ${event.start}
+  // End Time ${event.end}
+  // Location (to be figured out) `);
+  // };
 
   render() {
     return (
-      
       <div className="calendarWrapper">
         <Calendar
-        selectable
-        events={this.state.events}
-        showMultiDayTimes
-        localizer={localizer}
-        step={15}
+          selectable
+          events={this.state.events}
+          showMultiDayTimes
+          localizer={localizer}
+          step={15}
+          onSelectEvent={event => alert(`Start Time ${event.start}
+          End Time ${event.end}
+          Location "("to be figured out")"`)}
         />
-
       </div>
     );
   }
